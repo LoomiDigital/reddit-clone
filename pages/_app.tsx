c103fa72e6a29c47e189d64591c83b989cc3cde5
Iñigo Villalba <inigo@loomi.io>
2023-04-06 15:37:28 +0200

    Include Header in _App.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
 import "@d20/styles/globals.css";
+import Header from "@d20/Components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <div className="h-screen overflow-y-scroll bg-slate-200">
+        <Header />
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 }
