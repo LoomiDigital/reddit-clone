74f506658e8b8c70fc3eb1968df72b7d848058aa
Iñigo Villalba <inigo@loomi.io>
2023-04-13 18:17:38 +0200

    Fix logo vertical alignment

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
   const { data: session } = useSession();
 
   return (
-    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm ">
+    <div className="sticky top-0 z-50 flex items-center bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image
           className="object-contain"
