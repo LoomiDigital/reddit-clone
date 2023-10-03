9355cda3be73717663485eabb2958e8eec8dc6fc
Iñigo Villalba <inigo@loomi.io>
2023-10-03 14:45:26 +0200

    Move Comments into their own component.
    Create the unit test

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -5,11 +5,8 @@ import {
   PostAttributesFragment,
 } from "@d20/generated/graphql";
 
-import { CommentLoader } from "@d20/Components/Loaders";
 import PostCard from "@d20/Components/PostCard";
-
-import CommentCard from "@d20/Components/CommentCard";
-import { useAddComment } from "@d20/hooks/useAddComment";
+import Comments from "@d20/Components/Comments";
 
 type Params = {
   postId: string;
@@ -20,46 +17,10 @@ type Props = {
 };
 
 function PostPage({ post }: Props) {
-  const { submitComment, loading, comments, session, isValid, register } =
-    useAddComment(post);
-
   return (
     <div className="mx-auto my-7 max-w-5xl">
       <PostCard post={post} />
-      <div className="mt-5 rounded-t-md border border-b-0 border-t-0 border-gray-300 bg-white p-5 pl-16">
-        <p className="text-sm">
-          Comments as{" "}
-          <span className="text-red-500">{session?.user?.name}</span>
-        </p>
-        <form onSubmit={submitComment} className="flex flex-col space-y-2">
-          <textarea
-            disabled={!session}
-            {...register("comment", { required: true })}
-            className="h-24 rounded-sm border border-gray-200 p-2 pl-4 outline-none focus-within:border-gray-900 disabled:bg-gray-50"
-            placeholder={
-              session ? "What are your thoughts?" : "Please log in to comment"
-            }
-          ></textarea>
-          <button
-            disabled={!isValid || !session}
-            type="submit"
-            name="comment"
-            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:cursor-default disabled:bg-gray-500 disabled:text-gray-300"
-          >
-            Comment
-          </button>
-        </form>
-      </div>
-      <div className="rounded-b-md border border-t-0 border-gray-300 bg-white px-10 py-5">
-        <hr className="py-2" />
-        {loading ? (
-          <CommentLoader length={10} />
-        ) : (
-          comments?.map((comment) => (
-            <CommentCard key={comment?.id} comment={comment!} />
-          ))
-        )}
-      </div>
+      <Comments post={post} />
     </div>
   );
 }
